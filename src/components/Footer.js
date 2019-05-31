afaad6901a16c269d3d7ad23aed28781ad24b159
Rafael-LH <rafael.iskoat@gmail.com>
2019-05-31 14:25:31 -0500

    proyecto casi terminado

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,27 @@ import React,{Component} from 'react'
 
     const Footer = () => (
         <div className="footer">
-            <h1>Footer</h1>
+            <div className="sub-section-footer">
+                <h1 className="title-social">Technologies</h1>
+                <ul>
+                    <li>HTML 5</li>
+                    <li>Sass</li>
+                    <li>Webpack</li>
+                    <li>ReactJS</li>
+                </ul>
+                <h1 className="title-social">Api</h1>
+                <ul>
+                    <li>Api GitHub</li>
+                </ul>
+                <h1 className="title-social">Networks</h1>
+                <ul>
+                    <li><a href="github.com/Rafael-LH">GitHub<i class="fab fa-github"></i></a></li>
+                    <li><a href="https://github.com/Rafael-LH?tab=repositories">BitBucket<i class="fab fa-buysellads"></i> </a></li>
+                </ul>
+            </div>
+            <div className="down-section">
+                 <p>About me, Todos los derechos © Rafael Lòpez 2019</p>
+            </div>
         </div>
     )
     export default Footer
--- src/components/Information.js
+++ src/components/Information.js
@@ -2,10 +2,15 @@ import React,{Component} from 'react'
 
     const Information = () => (
          <div className="container-about-me" id="information">
-            <p>
-                Information Information Information Information
-                Information Information Information Information
-            </p>
+             <img src="" alt=""/>
+             <ul>
+                 <li><b>First name</b> Rafael Iskoat</li>
+                 <li><b>Last name</b> Lopez herrera</li>
+                 <li><b>Phone</b></li>
+                 <li><b>Cel phone</b> 33 35 96 09 55</li>
+                 <li><b>Addres</b> Fraccionamiendo las gabrielas nube 58 tonala</li>
+                 <li><b>Language</b> Espanish (native) English (in proces)</li>
+             </ul>
          </div>
     )
     
